6bc4b5ea1e5f3d907fc662ca3b78d9c1d992b5e0
cotton1021 <cot.1021.n@gmail.com>
2025-03-02 17:49:56 +0900

    ToDo：新規登録時に2件登録されてしまう不具合を修正

--- inc/do/add_todo.php
+++ inc/do/add_todo.php
@@ -130,9 +130,9 @@ if (!empty($_POST['todo_id']) && $todo_complete == 1) {
 		$data[] = $todo_note;
 		$data[] = $todo_create_date;
 	}
+	$stmt->execute($data);
 }
 
-$stmt->execute($data);
 $dbh = null;
 
 $todourl = TODO_URL;
